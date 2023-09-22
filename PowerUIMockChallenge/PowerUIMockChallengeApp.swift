673165d320733742d8b5aaeb215fb640c908d1bf
Rachel Radford <rachelradford21@gmail.com>
2023-09-22 18:27:40 -0400

    Initial Commit

---
+++ PowerUIMockChallenge/PowerUIMockChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerUIMockChallengeApp.swift
+//  PowerUIMockChallenge
+//
+//  Created by Rachel Radford on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct PowerUIMockChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
