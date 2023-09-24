ab94e68da9ea75231c3c6b3eeb546982d65d6052
Rachel Radford <rachelradford21@gmail.com>
2023-09-24 16:11:45 -0400

    Commit

--- PowerUIMockChallenge/View/TopCardPage/SocialMediaView.swift
+++ PowerUIMockChallenge/View/TopCardPage/SocialMediaView.swift
@@ -11,7 +11,7 @@ struct SocialMediaView: View {
     var number = ""
     var socialText = ""
     var body: some View {
-        // With these social media stats formatted as a single string you cannot change the font size and color to match the mockup. So I changed it to two separate Text(). I also noticed Text() that had .multilineTextAlignment() but that should only be used for stings of text that are longer than a single line.
+        // With these social media stats formatted as a single string you cannot change the font size and color to match the mockup. So I changed it to two separate Text(). I also noticed Text() that had .multilineTextAlignment() but that should only be used for strings of text that are longer than a single line.
         
         Text(number)
             .font(
